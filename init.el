4627b6743f195897608bbc68b2d38bf0167be9ac
James Hyun <jameshyun1230@gmail.com>
2020-07-08 01:40:05 -0400

    Backtab keybind

--- init.el
+++ init.el
@@ -18,6 +18,20 @@
 (setq sgml-basic-offset 4)
 (setq lua-indent-level 4)
 
+;; Backtab
+(global-set-key (kbd "<backtab>") 'un-indent-by-removing-4-spaces)
+(defun un-indent-by-removing-4-spaces ()
+  "remove 4 spaces from beginning of of line"
+  (interactive)
+  (save-excursion
+    (save-match-data
+      (beginning-of-line)
+      ;; get rid of tabs at beginning of line
+      (when (looking-at "^\\s-+")
+        (untabify (match-beginning 0) (match-end 0)))
+      (when (looking-at "^    ")
+                (replace-match "")))))
+
 ;; Green comments
 (set-face-foreground 'font-lock-comment-face "green")
 
